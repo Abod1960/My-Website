d7f5d8cf3d9444bf6c600cd3013f301c4a9ab9e7
Abdullah Omar <79435005+Abod1960@users.noreply.github.com>
2021-08-30 15:51:01 +0400

    update

--- js/fade logo caption.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById('id').classList.toggle('fade-out');
-  } else {
-    document.getElementById('id').classList.toggle('fade-out');
-  }
-}
