a9e41ab961f6e53182ad022476b3a9259c8d2d3f
Abdullah Omar <aljumaili2005@gmail.com>
2022-08-02 14:26:25 +0400

    minor updates

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,18 @@ $(document).ready(function(){
   
 // });
 
+$('.with-caption').magnificPopup({
+  type: 'image',
+  closeOnContentClick: true,
+  closeBtnInside: false,
+  mainClass: 'mfp-with-zoom mfp-img-mobile',
+  image: {
+    verticalFit: true,
+    titleSrc: function(item) {
+      return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+    }
+  },
+  zoom: {
+    enabled: true
+  }
+});
